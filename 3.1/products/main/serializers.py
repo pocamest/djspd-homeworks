4ba39080ce6c6dcb2b58eb2c2d146744b7d8b543
pocamest <pocamest@yandex.ru>
2025-02-19 02:45:02 +0300

    Added views

--- 3.1/products/main/serializers.py
+++ 3.1/products/main/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
+from .models import Product, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # реализуйте все поля
-    pass
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class ProductListSerializer(serializers.Serializer):
-    # реализуйте поля title и price
-    pass
+    title = serializers.CharField(max_length=100)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
-    # реализуйте поля title, description, price и reviews (список отзывов к товару)
-    pass
+    comments = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'comments']
--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -1,25 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
+from main.models import Product
 
 
 @api_view(['GET'])
 def products_list_view(request):
-    """реализуйте получение всех товаров из БД
-    реализуйте сериализацию полученных данных
-    отдайте отсериализованные данные в Response"""
-    pass
+    products = Product.objects.all()
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
-        """реализуйте получение товара по id, если его нет, то выдайте 404
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        product = get_object_or_404(Product, id=product_id)
+        serializer = ProductDetailsSerializer(product)
+        return Response(serializer.data)
 
 
 # доп задание:
